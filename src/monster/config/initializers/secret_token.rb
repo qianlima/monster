faf0b2be8b3c087f381793460260c7448b968393
qianlima <worksametime@hotmail.com>
2014-05-12 18:50:54 -1000

    Initial commit

---
+++ src/monster/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monster::Application.config.secret_key_base = '4bdd14d239a10eb8ad2049e77e23d80311d665aabe76ec8ed85af3a4638ac40bf62b8c24cda9bcd3630dd0fac97b23645ea8ab2fefa5ed9a124e977f63e0f654'
---
+++ src/monster/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Monster::Application.config.session_store :cookie_store, key: '_monster_session'
